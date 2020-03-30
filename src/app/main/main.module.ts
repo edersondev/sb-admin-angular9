93e3ff5c7cfa473b9b9138068c3be108198c267a
Ederson Ferreira <ederson.dev@gmail.com>
2020-03-30 00:24:20 -0300

    atualização do main module

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,23 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainComponent } from './main.component';
-import { TranslateModule } from '@ngx-translate/core';
-import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { MainRoutingModule } from './main-routing.module';
-import { LayoutComponent } from '../layout/layout.component';
-import { SidebarComponent } from '../layout/components/sidebar/sidebar.component';
-import { HeaderComponent } from '../layout/components/header/header.component';
+import { LayoutModule } from '../layout/layout.module';
 
 @NgModule({
   declarations: [
-    MainComponent,
-    LayoutComponent, SidebarComponent, HeaderComponent
+    
   ],
   imports: [
     CommonModule,
     MainRoutingModule,
-    TranslateModule,
-    NgbDropdownModule
+    LayoutModule
   ]
 })
 export class MainModule { }
