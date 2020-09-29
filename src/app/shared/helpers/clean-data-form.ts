8f3b402a950415f05955d23900b8f273e25a1e04
Ederson Ferreira <ederson.dev@gmail.com>
2020-09-29 14:26:35 -0300

    Create clean-data-form.ts
    delete item from object if is null or undefined or empty string

---
+++ src/app/shared/helpers/clean-data-form.ts
@@ -0,0 +1,8 @@
+export function CleanDataForm(obj:any){
+  for (var propName in obj) {
+    if (obj[propName] === null || obj[propName] === undefined || obj[propName] === "") {
+      delete obj[propName];
+    }
+  }
+  return obj;
+}
