16cafed1a857a61f4d8cc2a108acfe8673ef92f1
Ederson Ferreira <ederson.dev@gmail.com>
2021-04-08 17:49:44 -0300

    Update resource-service.ts

--- src/app/shared/resource/resource-service.ts
+++ src/app/shared/resource/resource-service.ts
@@ -6,68 +6,79 @@ export abstract class ResourceService<T> {
 
   private url:string = '';
 
-  httpClient: HttpClient;
   endpoint: string;
   sufixUrl:string;
-  urlRest:string;
 
-  setParams(
-    httpClient: HttpClient,
-    endpoint: string,
-    sufixUrl?:string
-  ):void {
-    this.httpClient = httpClient;
-    this.endpoint = endpoint;
-    this.sufixUrl = (sufixUrl === undefined ? 'rest/s' : sufixUrl);
-    this.urlRest = `${this.url}/${this.sufixUrl}/${this.endpoint}`;
+  constructor(
+    private _httpClient: HttpClient,
+    private _dialog?: DialogService
+  ) { }
+
+  /**
+   * Definição das variaveis endpoint e sufixUfl
+   */
+  abstract setEndPoint(endpoint: string,sufixUrl?:string): void;
+  
+  getUrlRequest(): string {
+    if(this.endpoint === undefined){
+      throw new Error('Endpoint is undefined');
+    }
+    let sufixUrl = (this.sufixUrl === undefined ? 'rest/s' : this.sufixUrl);
+    return `${this.url}${sufixUrl}/${this.endpoint}`;
   }
 
   findAll(objParams?:any):Observable<T>{
+    let urlRest = this.getUrlRequest();
     const options = {
       params: new HttpParams({fromObject:objParams})
     };
-    return this.httpClient.get<T>(this.urlRest,options)
+    return this.httpClient.get<T>(urlRest,options)
       .pipe(
         catchError(this.handleError)
       );
   }
 
   find(id:number):Observable<T>{
-    return this.httpClient.get<T>(`${this.urlRest}/${id}`)
+    let urlRest = this.getUrlRequest();
+    return this.httpClient.get<T>(`${urlRest}/${id}`)
     .pipe(
       catchError(this.handleError)
     );
   }
 
   create(dataPost:any):Observable<T>{
-    return this.httpClient.post<T>(this.urlRest,dataPost)
+    let urlRest = this.getUrlRequest();
+    return this.httpClient.post<T>(urlRest,dataPost)
     .pipe(
       catchError(this.handleError)
     );
   }
 
   update(id:number,dataPost:any):Observable<T>{
-    return this.httpClient.put<T>(`${this.urlRest}/${id}`,dataPost)
+    let urlRest = this.getUrlRequest();
+    return this.httpClient.put<T>(`${urlRest}/${id}`,dataPost)
     .pipe(
       catchError(this.handleError)
     );
   }
 
   createOrUpdate(dataPost:any, id?:number):Observable<T>{
+    let urlRest = this.getUrlRequest();
     if(id === undefined){
-      return this.httpClient.post<T>(this.urlRest,dataPost)
+      return this.httpClient.post<T>(urlRest,dataPost)
       .pipe(
         catchError(this.handleError)
       );
     }
-    return this.httpClient.put<T>(`${this.urlRest}/${id}`,dataPost)
+    return this.httpClient.put<T>(`${urlRest}/${id}`,dataPost)
     .pipe(
       catchError(this.handleError)
     );
   }
 
   delete(id:number):Observable<T>{
-    return this.httpClient.delete<T>(`${this.urlRest}/${id}`)
+    let urlRest = this.getUrlRequest();
+    return this.httpClient.delete<T>(`${urlRest}/${id}`)
     .pipe(
       catchError(this.handleError)
     );
